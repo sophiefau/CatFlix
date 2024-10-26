bec43382609d768543937eca498716bc87c4f480
sophiefau <sophiefauquembergue@gmail.com>
2024-10-26 21:32:40 +0200

    correct swagger error

--- swagger.js
+++ swagger.js
@@ -2,21 +2,26 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
 const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'CatFlix API',
-      version: '1.0.0',
-      description: 'API documentation for CatFlix, an app featuring movies with cats!',
-    },
-    servers: [
-      {
-        url: 'http://localhost:8080', // Local development
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Catflix API',
+            version: '1.0.0',
+            description: 'API documentation for CatFlix, an app featuring movies with cats!'
+        },
       },
-      {
-        url: 'https://catflix-99a985e6fffa.herokuapp.com', // Heroku deployment
-      },
-    ],
-  },
-  apis: ['./index.js'],
+      servers: [
+        {
+          url: 'http://localhost:8080',
+        },
+        {
+          url: 'https://catflix-99a985e6fffa.herokuapp.com',
+        },
+      ],
+    apis: ['./index.js'],
 };
+
+// Initialize swagger-jsdoc
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = { swaggerUi, swaggerDocs };
