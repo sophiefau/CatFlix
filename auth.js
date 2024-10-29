516900e4c7d9bd22a0e2a07476c6f9618f1228cd
sophiefau <sophiefauquembergue@gmail.com>
2024-10-29 19:59:33 +0100

    add /login to swagger

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 const jwtSecret = "your_jwt_secret"; // This has to be the same key used in the JWTStrategy
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const { swaggerUi, swaggerDocs } = require("./swagger");
 
 require("./passport"); // Your local passport file
 
